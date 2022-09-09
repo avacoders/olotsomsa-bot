0e9058e97a9bbb4f8d9b4ffb57e8dc25fd4343d2
Avazbek <avazbeknematov01@gmail.com>
2022-09-09 23:41:16 +0500

    mobile exception

--- database/migrations/2022_06_24_193711_change_email_column_to_users_table.php
+++ database/migrations/2022_06_24_193711_change_email_column_to_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->string('email')->nullable()->change();
-            $table->bigInteger('telegram_id')->nullable();
+            $table->bigInteger('telegram_id')->unique();
         });
     }
 
