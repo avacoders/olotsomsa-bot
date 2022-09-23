56452c57d7bcdf306380849456d7c942816c9737
Avazbek <avazbeknematov01@gmail.com>
2022-09-23 22:58:57 +0500

    optimize

--- app/Helpers/Telegram.php
+++ app/Helpers/Telegram.php
@@ -1179,9 +1179,10 @@ class Telegram
 
     public function validatePhoneNumber($user, $phone_number)
     {
-
         $contact = preg_replace('/[^0-9.]+/', '', $phone_number);
         $code5 =  substr($contact, 0, 5);
+        $code5 = preg_replace('/[^0-9.]+/', '', $code5);
+
 
         if (strlen($contact) != 12 || !in_array($code5, [99890, 99891, 99893, 99894, 99895, 99897, 99899]) || $code5 == 99898) {
             $this->sendMessage($user->telegram_id, "Iltimos, telefon raqamni to'g'ri kiriting! Masalan: 9989012345678");
