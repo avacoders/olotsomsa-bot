a18b530e76a8c0f19c2d4941ba339bdc9bb81601
Avazbek <avazbeknematov01@gmail.com>
2022-09-24 01:17:30 +0500

    optimize

--- database/migrations/2022_09_24_010300_change_columns_to_users_table.php
+++ database/migrations/2022_09_24_010300_change_columns_to_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->string('email')->nullable()->change();
-            $table->bigInteger('telegram_id')->unique();
+            $table->bigInteger('telegram_id')->default(0);
         });
     }
 
