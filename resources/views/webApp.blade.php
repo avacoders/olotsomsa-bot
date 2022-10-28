87cfc86f19abc70091a771ba8b55946202aa3f78
Avazbek <avazbeknematov01@gmail.com>
2022-10-28 20:03:28 +0500

    optimize

--- resources/views/webApp.blade.php
+++ resources/views/webApp.blade.php
@@ -5,9 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
-          integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <link href="{{ asset("css/aos.css") }}" rel="stylesheet">
     <script src="https://telegram.org/js/telegram-web-app.js"></script>
 
     <style>
@@ -168,216 +167,231 @@
 </div>
 
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="{{ asset("js/jquery.js") }}"></script>
 
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
-        crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js"
-        integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2"
-        crossorigin="anonymous"></script>
-<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script src="{{ asset("js/popper.js") }}"></script>
+<script src="{{ asset("js/bootstrap.js") }}"></script>
+<script src="{{ asset("js/aos.js") }}"></script>
 
 <script src="{{ asset("js/axios.js") }}"></script>
 
 <script>
-    var domain = "https://olotsomsa.com";
-    var selecting = null
-    var selected = []
-    var products = getProducts()
-    var quantity = 1
-    var mainBtn = window.Telegram.WebApp.MainButton
+    AOS.init();
+    $(document).ready(function() {
+
+        var domain = "https://olotsomsa.com";
+        var selecting = null
+        var selected = []
+        var products = getProducts()
+        var quantity = 1
+        var mainBtn = window.Telegram.WebApp.MainButton
 
 
+        makeProductsUI(products)
 
-    makeProductsUI(products)
+        function makeProductsUI(products) {
+            var div = ""
+            var item = false
+            var btnClass
+            for (let i = 0; i < products.length; i++) {
+                item = products[i]
+                btnClass = selected[item.id] ? "btn-warning" : "btn-success"
+                btnText = selected[item.id] ? "OLIB TASHLASH" : "QO'SHISH";
 
-    function makeProductsUI(products) {
-        var div = ""
-        var item = false
-        var btnClass
-        for (let i = 0; i < products.length; i++) {
-            item = products[i]
-            btnClass = selected[item.id] ? "btn-warning" : "btn-success"
-            btnText = selected[item.id] ? "OLIB TASHLASH" : "QO'SHISH";
+                div += '<div class="col-4 position-relative">'
+                div += '<div class="py-3" style="height: 250px">'
+                div += '<div class="d-none counter" id="counter' + item.id + '">2</div>'
+                div += '<img  data-toggle="modal" data-target="#exampleModal" data-id="' + item.id + '"  src="' + item.image + '" class="product-img " alt="">'
+                div += '<h6 class="text-center mt-3 mx-auto">' + item.name + ' <br> ' + item.price + ' so\'m</h6>'
+                div += '<button   data-toggle="modal" data-target="#exampleModal" data-id="' + item.id + '" id="btn' + item.id + '" class="btn add ' + btnClass + '">'
+                div += "QO'SHISH"
+                div += "</button>"
+                div += '</div>'
+                div += '</div>'
+            }
 
-            div += '<div class="col-4 position-relative">'
-            div += '<div class="py-3" style="height: 250px">'
-            div += '<div class="d-none counter" id="counter' + item.id + '">2</div>'
-            div += '<img src="' + item.image + '" class="product-img" alt="">'
-            div += '<h6 class="text-center mt-3 mx-auto">' + item.name + ' <br> ' + item.price + ' so\'m</h6>'
-            div += '<button   data-toggle="modal" data-target="#exampleModal" data-id="' + item.id + '" id="btn' + item.id + '" class="btn add ' + btnClass + '">'
-            div += "QO'SHISH"
-            div += "</button>"
-            div += '</div>'
-            div += '</div>'
+
+            $("#products").html(div)
+        }
+
+        function calculate() {
+            sum = 0
+            for (let i = 0; i < selected.length; i++) {
+                if (selected[i] && selected[i].quantity)
+                    sum += selected[i].quantity * getProductByID(i).price
+            }
+            return sum
+        }
+
+        function changeCounter(id) {
+            $("#counter" + id).toggleClass('d-none')
+            $("#counter" + id).text(selected[id] ? selected[id].quantity : 1)
+        }
+
+        function changeBtn(id) {
+            btnText = selected[id] ? "-" : "QO'SHISH";
+            btnClass = selected[id] ? "btn-warning" : "btn-success"
+            btnClass2 = !selected[id] ? "btn-success" : "btn-warning"
+            $("#btn" + id).text(btnText)
+
         }
 
 
-        $("#products").html(div)
-    }
+        function changeMainBtn() {
+            var color = selected.length > 1 ? "#37D200" : "rgb(255, 193, 7)"
+            mainBtn.setParams({
+                "text": "Umumiy " + calculate() + " so'm",
+                "color": color,
+                "text_color": "#FFFFFF",
 
-    function calculate() {
-        sum = 0
-        for (let i = 0; i < selected.length; i++) {
-            if (selected[i] && selected[i].quantity)
-                sum += selected[i].quantity * getProductByID(i).price
+            })
         }
-        return sum
-    }
 
-    function changeCounter(id) {
-        $("#counter" + id).toggleClass('d-none')
-        $("#counter" + id).text(selected[id] ? selected[id].quantity : 1)
-    }
-
-    function changeBtn(id) {
-        btnText = selected[id] ? "-" : "QO'SHISH";
-        btnClass = selected[id] ? "btn-warning" : "btn-success"
-        btnClass2 = !selected[id] ? "btn-success" : "btn-warning"
-        $("#btn" + id).text(btnText)
-
-    }
-
-
-    function changeMainBtn() {
-        var color = selected.length > 1 ? "#37D200" : "rgb(255, 193, 7)"
+        mainBtn.show()
         mainBtn.setParams({
-            "text": "Umumiy " + calculate() + " so'm",
-            "color": color,
+            "text": "Umumiy " + calculate() + " So'm",
+            "color": "rgb(255, 193, 7)",
             "text_color": "#FFFFFF",
 
         })
-    }
 
-    mainBtn.show()
-    mainBtn.setParams({
-        "text": "Umumiy " + calculate() + " So'm",
-        "color": "rgb(255, 193, 7)",
-        "text_color": "#FFFFFF",
-
-    })
-
-    mainBtn.onClick(() => {
-        if (selected.length > 1) {
-            axios.post('/api/bot', {
-                query_id: window.Telegram.WebApp.initDataUnsafe.query_id,
-                user: window.Telegram.WebApp.initDataUnsafe.user,
-                orders: selected
-            })
-                .then(response => {
-                    if (response.data.ok) window.Telegram.WebApp.close()
+        mainBtn.onClick(() => {
+            if (selected.length > 1) {
+                axios.post('/api/bot', {
+                    query_id: window.Telegram.WebApp.initDataUnsafe.query_id,
+                    user: window.Telegram.WebApp.initDataUnsafe.user,
+                    orders: selected
                 })
-        }
-
-    })
-    $('.add').click(function () {
-        selecting = getProductByID($(this).data('id'))
-        if (selecting) {
-            $("#name").text(selecting.name)
-            if (selecting.id == 19)
-            {
-                quantity = 10
-                $("#counter").text(10)
-                $("#general").text(selecting.price * 10)
-
-            }
-            else
-            {
-                $("#counter").text(1)
-                $("#general").text(selecting.price)
-
+                    .then(response => {
+                        if (response.data.ok) window.Telegram.WebApp.close()
+                    })
             }
 
-            $("#image").attr('src', selecting.image)
-            $("#price").text(selecting.price)
-        }
+        })
+        $('.add').click(function () {
+            selecting = getProductByID($(this).data('id'))
+            if (selecting) {
+                $("#name").text(selecting.name)
+                if (selecting.id == 19) {
+                    quantity = 10
+                    $("#counter").text(10)
+                    $("#general").text(selecting.price * 10)
 
-    })
-
-    $(".btn-plus").click(function () {
-        quantity++
-        $("#counter").text(quantity)
-        $("#general").text(quantity * selecting.price)
-    })
-
-    $(".btn-minus").click(function () {
-        if (quantity > 1)
-            quantity--
-        if (selecting.id == 19)
-        {
-            if (quantity < 10)
-            {
-                quantity = 10
-            }
-        }
-        $("#counter").text(quantity)
-        $("#general").text(quantity * selecting.price)
-
-    })
-
-    $(".submit").click(function () {
-        selected[selecting.id] = {
-            quantity
-        }
-        changeCounter(selecting.id)
-        changeBtn(selecting.id)
-        changeMainBtn()
-    })
-    $(".cancel").click(function () {
-        selected.splice(selecting.id)
-        console.log(selected);
-        changeCounter(selecting.id)
-        changeBtn(selecting.id)
-        changeMainBtn()
-
-    })
-
-
-    function getProductByID(id) {
-        for (let i = 0; i < products.length; i++) {
-            if (products[i].id === parseInt(id))
-                return products[i]
-        }
-        return false
-    }
-
-
-    function getProducts() {
-        var result = false
-
-        $.ajax({
-            url: domain + "/api/products/",
-            dataType: "json",
-            type: "get",
-            async: false,
-            success: function (data) {
-                result = data.data
-            },
-            error: function (xhr, exception) {
-                var msg = "";
-                if (xhr.status === 0) {
-                    msg = "Not connect.\n Verify Network." + xhr.responseText;
-                } else if (xhr.status == 404) {
-                    msg = "Requested page not found. [404]" + xhr.responseText;
-                } else if (xhr.status == 500) {
-                    msg = "Internal Server Error [500]." + xhr.responseText;
-                } else if (exception === "parsererror") {
-                    msg = "Requested JSON parse failed.";
-                } else if (exception === "timeout") {
-                    msg = "Time out error." + xhr.responseText;
-                } else if (exception === "abort") {
-                    msg = "Ajax request aborted.";
                 } else {
-                    msg = "Error:" + xhr.status + " " + xhr.responseText;
+                    $("#counter").text(1)
+                    $("#general").text(selecting.price)
+
                 }
 
+                $("#image").attr('src', selecting.image)
+                $("#price").text(selecting.price)
             }
-        });
+
+        })
+        $('.product-img').click(function () {
+            selecting = getProductByID($(this).data('id'))
+            if (selecting) {
+                $("#name").text(selecting.name)
+                if (selecting.id == 19) {
+                    quantity = 10
+                    $("#counter").text(10)
+                    $("#general").text(selecting.price * 10)
+
+                } else {
+                    $("#counter").text(1)
+                    $("#general").text(selecting.price)
+
+                }
+
+                $("#image").attr('src', selecting.image)
+                $("#price").text(selecting.price)
+            }
+
+        })
+
+        $(".btn-plus").click(function () {
+            quantity++
+            $("#counter").text(quantity)
+            $("#general").text(quantity * selecting.price)
+        })
+
+        $(".btn-minus").click(function () {
+            if (quantity > 1)
+                quantity--
+            if (selecting.id == 19) {
+                if (quantity < 10) {
+                    quantity = 10
+                }
+            }
+            $("#counter").text(quantity)
+            $("#general").text(quantity * selecting.price)
+
+        })
+
+        $(".submit").click(function () {
+            selected[selecting.id] = {
+                quantity
+            }
+            changeCounter(selecting.id)
+            changeBtn(selecting.id)
+            changeMainBtn()
+        })
+        $(".cancel").click(function () {
+            selected.splice(selecting.id)
+            console.log(selected);
+            changeCounter(selecting.id)
+            changeBtn(selecting.id)
+            changeMainBtn()
+
+        })
 
 
-        return result
-    }
+        function getProductByID(id) {
+            for (let i = 0; i < products.length; i++) {
+                if (products[i].id === parseInt(id))
+                    return products[i]
+            }
+            return false
+        }
+
+
+        function getProducts() {
+            var result = false
+
+            $.ajax({
+                url: domain + "/api/products/",
+                dataType: "json",
+                type: "get",
+                async: false,
+                success: function (data) {
+                    result = data.data
+                },
+                error: function (xhr, exception) {
+                    var msg = "";
+                    if (xhr.status === 0) {
+                        msg = "Not connect.\n Verify Network." + xhr.responseText;
+                    } else if (xhr.status == 404) {
+                        msg = "Requested page not found. [404]" + xhr.responseText;
+                    } else if (xhr.status == 500) {
+                        msg = "Internal Server Error [500]." + xhr.responseText;
+                    } else if (exception === "parsererror") {
+                        msg = "Requested JSON parse failed.";
+                    } else if (exception === "timeout") {
+                        msg = "Time out error." + xhr.responseText;
+                    } else if (exception === "abort") {
+                        msg = "Ajax request aborted.";
+                    } else {
+                        msg = "Error:" + xhr.status + " " + xhr.responseText;
+                    }
+
+                }
+            });
+
+
+            return result
+        }
+
+    })
 
 
 </script>
