d37251f140ace6290eea35ee4e550f7d75341242
Avazbek <avazbeknematov01@gmail.com>
2022-10-29 18:44:52 +0500

    optimize

--- resources/views/webApp.blade.php
+++ resources/views/webApp.blade.php
@@ -134,6 +134,7 @@
             position: fixed;
             left: 0;
             right: 0;
+            bottom: 0;
             background-color: #24303e;
             color: white;
             padding: 10px;
